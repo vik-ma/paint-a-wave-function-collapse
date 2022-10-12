7dd49226cb4e5ab117cdaebdfc60367471b3580b
Viktor <vikt.mag@gmail.com>
2022-10-12 21:19:47 +0200

    Made simple grid

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 
@@ -8,16 +9,30 @@ HEIGHT = 640
 clock = pygame.time.Clock()
 FPS = 60
 
+ROWS = 10
+COLS = 10
+TILE_SIZE = 50
+
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+GREY = (175, 175, 175)
+
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 def draw_window():
-    screen.fill((255,255,255))
+    screen.fill(GREY)
+
+def draw_grid():
+    for row in range(ROWS):
+        for col in range(COLS):
+            pygame.draw.rect(screen, WHITE, pygame.Rect((col * TILE_SIZE + 50), (row * TILE_SIZE + 50), TILE_SIZE, TILE_SIZE))
 
 def main():
     run = True
     while run:
         clock.tick(FPS)
         draw_window()
+        draw_grid()
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
