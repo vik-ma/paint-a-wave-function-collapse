e9cfc89db20501a889ac53058eba25438e0d2503
Viktor <vikt.mag@gmail.com>
2022-10-18 07:51:18 +0200

    Added observe and propagate functions

--- main.py
+++ main.py
@@ -229,7 +229,7 @@ def get_shannon_entropy(position, coefficients, probability):
     entropy -= random.uniform(0, 0.1)
     return entropy
 
-def get_min_entropy_pos(coefficients, probability):
+def get_min_entropy_at_pos(coefficients, probability):
     """Return position of tile with the lowest entropy"""
     min_entropy = None
     min_entropy_pos = None
@@ -247,6 +247,65 @@ def get_min_entropy_pos(coefficients, probability):
 
     return min_entropy_pos
 
+def observe(coefficients, probability):
+    # Find the lowest entropy
+    min_entropy_pos = get_min_entropy_at_pos()
+    
+    if min_entropy_pos == None:
+        print("All tiles have 0 entropy")
+        return
+    
+    # Choose a pattern at lowest entropy position which is most frequent in the sample
+    possible_patterns = get_possible_patterns_at_position(min_entropy_pos)
+    
+    # calculate max probability for patterns that are left
+    max_p = 0
+    for pattern in possible_patterns:
+        if max_p < probability[pattern]:
+            max_p == probability[pattern]
+    
+    
+    semi_random_pattern = random.choice([pat for pat in possible_patterns if probability[pat]>=max_p])
+    
+    # Set this pattern to be the only available at this position
+    coefficients[min_entropy_pos[0]][min_entropy_pos[1]] = semi_random_pattern
+    
+    return min_entropy_pos
+
+def propagate(min_entropy_pos, coefficients, rule_index):
+    stack = [min_entropy_pos]
+    
+    while len(stack) > 0:
+        pos = stack.pop()
+        
+        possible_patterns = get_possible_patterns_at_position(pos)
+        
+        # Iterate through each location immediately adjacent to the current location
+        for direction in get_valid_directions(pos):
+            adjacent_pos = (pos[0] + direction[0], pos[1] + direction[1])
+            possible_patterns_at_adjacent = get_possible_patterns_at_position(adjacent_pos)
+            
+            # Iterate over all still available patterns in adjacent tile 
+            # and check if pattern is still possible in this location
+            if not isinstance(possible_patterns_at_adjacent, list):
+                possible_patterns_at_adjacent = [possible_patterns_at_adjacent]
+            for possible_pattern_at_adjacent in possible_patterns_at_adjacent:
+                if len(possible_patterns) > 1:
+                    is_possible = any([rule_index.check_possibility(pattern, possible_pattern_at_adjacent, direction) for pattern in possible_patterns])
+                else:
+                    is_possible = rule_index.check_possibility(possible_patterns, possible_pattern_at_adjacent, direction)
+                    
+                """
+                If the tile is not compatible with any of the tiles in the current location's wavefunction
+                then it's impossible for it to ever get choosen so it needs to be removed from the other
+                location's wavefunction
+                """
+                if not is_possible:
+                    x, y = adjacent_pos
+                    coefficients[x][y] = [patt for patt in coefficients[x][y] if patt.pixels != possible_pattern_at_adjacent.pixels]
+                        
+                    if adjacent_pos not in stack:
+                        stack.append(adjacent_pos)
 
 def wave_function_collapse_main():
     pattern_list = get_pix_array_patterns(sample_pixel_array)
