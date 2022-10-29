edc6a7afbd73cd41b0d05214e00fe4f52180350f
Viktor <vikt.mag@gmail.com>
2022-10-29 20:24:09 +0200

    Changed names of col and row variables in Tile class to x and y

--- tile.py
+++ tile.py
@@ -1,7 +1,7 @@
 import pygame
 
 class Tile(pygame.sprite.Sprite):
-    def __init__(self, width, height, col, row, pix_array):
+    def __init__(self, width, height, x, y, pix_array):
         pygame.sprite.Sprite.__init__(self)
 
         self.image = pygame.Surface([width, height])
@@ -13,7 +13,7 @@ class Tile(pygame.sprite.Sprite):
         pixel_array.close()
 
         self.image = pygame.transform.scale(self.image, ((width*10), (height*10)))        
-        self.col = col
-        self.row = row
+        self.x = x
+        self.y = y
 
-        self.rect = self.image.get_rect(left = self.col, top = self.row)
+        self.rect = self.image.get_rect(left = self.x, top = self.y)
