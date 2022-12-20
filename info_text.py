f30c16eb79ff9be7328bc5da8d42e0da4f833cd2
Viktor <vikt.mag@gmail.com>
2022-12-20 22:11:08 +0100

    Added new variables for InfoText class

--- info_text.py
+++ info_text.py
@@ -2,11 +2,13 @@ import pygame
 
 
 class InfoText():
-    def __init__(self, x, y, width, height):
+    def __init__(self, x, y, main_text, hover_box_text, hover_box_width, hover_box_height):
         self.x = x
         self.y = y
-        self.width = width
-        self.height = height
+        self.main_text = main_text
+        self.hover_box_text = hover_box_text
+        self.hover_box_width = hover_box_width
+        self.hover_box_height = hover_box_height
 
     def show(self, surface):
         pygame.draw.rect(surface, (0,0,0), (self.x, self.y, self.width + 1, self.height + 1), 1)
