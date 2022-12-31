7070c9c0ec4430b51ea99e6fffdad8631547a83b
Viktor <vikt.mag@gmail.com>
2022-12-31 22:21:52 +0100

    Made HoverBoxes appear at mouse position when hovering over InfoText objects

--- hover_box.py
+++ hover_box.py
@@ -3,39 +3,26 @@ import pygame
 class HoverBox(pygame.sprite.Sprite):
     def __init__(self, x, y, width, height, text, font):
         pygame.sprite.Sprite.__init__(self)
-        # self.x = x
-        # self.y = y
         self.width = width
         self.height = height
         self.font = font
-
-        self.hover_box_text = text
+        self.text_line_height = self.font.get_linesize()
+        self.text = []
+        for text_line in text:
+            self.text.append(self.font.render(text_line, True, (0, 0, 0)))
 
         self.update_image(x, y)
         
     def update_image(self, x, y):
-        self.text = []
-        for text_line in self.hover_box_text:
-            self.text.append(self.font.render(text_line, True, (0, 0, 0)))
-
-        self.text_line_height = self.font.get_linesize()
+        #Box
         self.rect = pygame.Rect(x, y, self.width, self.height)
         self.image = pygame.Surface([self.width, self.height])
         self.image.fill((255,255,255))
-        # self.pygame.draw.rect(surface, (255, 255, 255), self.rect)
+        
+        #Border
         pygame.draw.rect(self.image, (0,0,0), (0, 0, self.width, self.height), 1)
+        
+        #Text
         for line_y_pos, text_line in enumerate(self.text):
             hover_box_text_rect = text_line.get_rect(left = 14, top = (8 + (line_y_pos * self.text_line_height)))
             self.image.blit(text_line, hover_box_text_rect)
-
-
-    # def show(self, surface, x, y):
-    #     rect = pygame.Rect(x, y, self.width, self.height)
-    #     pygame.draw.rect(surface, (255, 255, 255), rect)
-
-    #     for line_y_pos, text_line in enumerate(self.text):
-    #         hover_box_text_rect = text_line.get_rect(left = x + 14, top = (y + 8 + (line_y_pos * self.text_line_height)))
-    #         surface.blit(text_line, hover_box_text_rect)
-
-    #     pygame.draw.rect(surface, (0,0,0), (x, y, self.width + 1, self.height + 1), 1)
-
--- info_text.py
+++ info_text.py
@@ -11,14 +11,17 @@ class InfoText():
         self.render_main_text = self.main_text_font.render(main_text, True, main_text_color)
         self.hover_box = hover_box
         self.hover_box_group = hover_box_group
+        self.is_showing_hover_box = False
 
-
-    def draw(self, surface, hover_box):
+    def draw(self, surface):
         pos = pygame.mouse.get_pos()
         surface.blit(self.render_main_text, (self.x, self.y))
 
         if self.rect.collidepoint(pos):
-            hover_box.x = pos[0]
-            hover_box.y = pos[1]
+            self.hover_box.update_image(pos[0], pos[1])
             self.hover_box_group.add(self.hover_box)
-            # self.hover_box.show(surface, pos[0], pos[1])
+            self.is_showing_hover_box = True
+        else:
+            if self.is_showing_hover_box:
+                self.hover_box_group.remove(self.hover_box)
+                self.is_showing_hover_box = False
