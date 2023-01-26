faf70ddad7e418b959160eefbc11cdd917119eb9
Viktor <vikt.mag@gmail.com>
2023-01-26 22:37:26 +0100

    Added docstring for PaintTile class

--- paint_tile.py
+++ paint_tile.py
@@ -1,6 +1,24 @@
 import pygame
 
 class PaintTile(pygame.sprite.Sprite):
+    """
+    This is a class for clickable, single colored paint tiles in the GUI of the pygame application.
+      
+    Attributes:
+        x (int): X-position of the tile.
+        y (int): Y-position of the tile.
+        width (int): Width of the tile.
+        height (int): Height of the tile.
+        color (tuple): Background color of the tile in RGB format.
+        image (pygame.Surface): Pygame Surface object to render tile in GUI.
+        rect (pygame.Rect): Pygame Rect object for rendering and collision detection.
+        clicked (bool): Bool to represent whether or not the tile is clicked.
+
+    Methods:
+        draw(self, surface, *, border=False)
+            Draws the tile on the pygame surface and returns True if button is clicked. 
+            Also draws a black border around the tile if border parameter is True.
+    """
     def __init__(self, width, height, x, y, color):
         self.x = x
         self.y = y
@@ -14,19 +32,19 @@ class PaintTile(pygame.sprite.Sprite):
         self.clicked = False
 
     def draw(self, surface, *, border=False):
-            action = False
-            pos = pygame.mouse.get_pos()
+        action = False
+        pos = pygame.mouse.get_pos()
 
-            if self.rect.collidepoint(pos):
-                if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
-                    self.clicked = True
-                    action = True
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+                self.clicked = True
+                action = True
 
-            if pygame.mouse.get_pressed()[0] == 0:
-                self.clicked = False
-                
-            surface.blit(self.image, (self.rect.x, self.rect.y))
-            if border:
-                pygame.draw.rect(surface, (0, 0, 0), (self.x - 1, self.y - 1, self.width + 2, self.height + 2), 1)
+        if pygame.mouse.get_pressed()[0] == 0:
+            self.clicked = False
+            
+        surface.blit(self.image, (self.rect.x, self.rect.y))
+        if border:
+            pygame.draw.rect(surface, (0, 0, 0), (self.x - 1, self.y - 1, self.width + 2, self.height + 2), 1)
 
-            return action
+        return action
