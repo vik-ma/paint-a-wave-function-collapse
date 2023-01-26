1b359853da8a0eff6cb1b46cb6b292840308b047
Viktor <vikt.mag@gmail.com>
2023-01-26 22:39:50 +0100

    Added docstring for Pattern class

--- pattern.py
+++ pattern.py
@@ -1,4 +1,10 @@
 class Pattern:
+    """
+    This is a class for the patterns extracted from the base tile.
+
+    Attributes:
+        pix_array (list): Two dimensonal array storing color data for every position in the Pattern. 
+    """
     def __init__(self, pix_array):
         self.pix_array = pix_array
         
