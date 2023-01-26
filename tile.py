5e21339062cdfab10d0c12a4aa01e7cc49cf8e17
Viktor <vikt.mag@gmail.com>
2023-01-26 23:03:40 +0100

    Added docstring for Tile class

--- tile.py
+++ tile.py
@@ -1,9 +1,24 @@
 import pygame
 
 class Tile(pygame.sprite.Sprite):
+    """
+    This is a class for the Base Tiles that gets painted and ultimately extracted for patterns for the WFC algorithm.
+
+    Attributes:
+        x (int): X-position of the Tile.
+        y (int): Y-position of the Tile.
+        width (int): Width of the Tile.
+        height (int): Height of the Tile.
+        image (pygame.Surface): Pygame Surface object to render Tile in GUI.
+        pix_array (list): Two dimensonal array storing color data for every position in the Tile.
+        rect (pygame.Rect): Pygame Rect object for rendering tile onto screen.
+    """
     def __init__(self, width, height, x, y, pix_array, enlargement_scale):
         pygame.sprite.Sprite.__init__(self)
-
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
         self.image = pygame.Surface([width, height])
         self.pix_array = pix_array
 
@@ -11,11 +26,5 @@ class Tile(pygame.sprite.Sprite):
             for j, color in enumerate(row):
                 self.image.set_at((i, j), color)
 
-        self.image = pygame.transform.scale(self.image, ((width*enlargement_scale), (height*enlargement_scale)))        
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.pix_array = pix_array
-
+        self.image = pygame.transform.scale(self.image, ((self.width*enlargement_scale), (self.height*enlargement_scale)))        
         self.rect = self.image.get_rect(left = self.x, top = self.y)
