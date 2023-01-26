30012449d2bd86a6e91526ba022bba6cb24b5d88
Viktor <vikt.mag@gmail.com>
2023-01-26 21:55:05 +0100

    Added docstring to SampleTile class

--- sample_tile.py
+++ sample_tile.py
@@ -1,4 +1,12 @@
 class SampleTile:
+    """
+    This is a class for the premade sample Base Tiles.
+      
+    Attributes:
+        pix_array (list): Two dimensonal array storing color data for every position in the Tile. 
+        width (int): Width of Tile (or Pixel Array).
+        height (int): Height of Tile (or Pixel Array).
+    """
     def __init__(self, pix_array, width, height):
         self.pix_array = pix_array
         self.width = width
