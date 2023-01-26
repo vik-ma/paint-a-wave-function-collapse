c6c7439882040b8223478423e4ee2906a894aff6
Viktor <vikt.mag@gmail.com>
2023-01-26 22:51:02 +0100

    Added docstring for RuleIndex class

--- rule_index.py
+++ rule_index.py
@@ -1,4 +1,16 @@
 class RuleIndex:
+    """
+    This is a class to store the rules for adjacent tiles in the wave function collapse algorithm.
+
+    Attributes:
+        data (dict): A dictionary that stores a list of patterns that are allowed at every relative position at current index.
+    
+    Methods:
+        add_rule(self, pattern, relative_position, next_pattern)
+            Adds next_pattern to the list of allowed patterns at relative postion of the current index.
+        check_possibility(self, pattern, check_pattern, relative_position)
+            Returns True if check_pattern is in stored list of allowed patterns, otherwise False.
+    """
     def __init__(self, pattern_list, directions):
         self.data = {}
         for pattern in pattern_list:
